76871653cbf860083210adc18a519946690237e5
alihpss <ali.hps@hotmail.com>
2024-08-29 11:23:14 -0300

    :sparkles: Feat: Create app tsx

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Router } from "./Router";
+import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./app/contexts/AuthContext";
+
+const queryClient = new QueryClient();
+
+export function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Router />
+
+        <Toaster />
+      </AuthProvider>
+    </QueryClientProvider>
+  );
+}
