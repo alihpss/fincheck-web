72ccaf2f74a007a283a88bb04f50bf0b6148b67a
Ali <ali.hps@hotmail.com>
2024-11-18 08:58:52 -0300

    ✨ Feat: Filters modal ui and hooks

---
+++ src/view/pages/Dashboard/components/Transactions/FiltersModal/useFiltersModal.ts
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export function useFiltersModal() {
+  const [selectedBankAccountId, setSelectedBankAccountId] = useState<
+    null | string
+  >(null);
+
+  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
+
+  function handleSelectBankAccountId(bankAccountId: string) {
+    setSelectedBankAccountId((prevState) =>
+      prevState === bankAccountId ? null : bankAccountId
+    );
+  }
+
+  function handleChangeYear(step: number) {
+    setSelectedYear((prevState) => prevState + step);
+  }
+
+  return {
+    handleSelectBankAccountId,
+    handleChangeYear,
+    selectedBankAccountId,
+    selectedYear,
+  };
+}
--- src/view/pages/Dashboard/components/Transactions/index.tsx
+++ src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -1,5 +1,3 @@
-import { ChevronDownIcon } from "@radix-ui/react-icons";
-import { TransactionsIcon } from "../../../../components/icons/TransactionsIcon";
 import { FilterIcon } from "../../../../components/icons/FilterIcon";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { MONTHS } from "../../../../../app/config/constants";
@@ -12,10 +10,19 @@ import { cn } from "../../../../../app/utils/cn";
 import { Spinner } from "../../../../components/Spinner";
 
 import emptyStateImage from "../../../../../assets/empty-state.svg";
+import { TransactionTypeDropdown } from "./TransactionTypeDropdown";
+import { FiltersModal } from "./FiltersModal";
 
 export function Transactions() {
-  const { areValuesVisible, isLoading, transactions, isInitialLoading } =
-    useTransactionsController();
+  const {
+    areValuesVisible,
+    isLoading,
+    transactions,
+    isInitialLoading,
+    handleCloseFiltersModal,
+    handleOpenFiltersModal,
+    isFiltersModalOpen,
+  } = useTransactionsController();
 
   const hasTransactions = transactions.length > 0;
 
@@ -29,17 +36,16 @@ export function Transactions() {
 
       {!isInitialLoading && (
         <>
+          <FiltersModal
+            open={isFiltersModalOpen}
+            onClose={handleCloseFiltersModal}
+          />
+
           <header>
             <div className="flex items-center justify-between">
-              <button className="flex items-center gap-2">
-                <TransactionsIcon />
-                <span className="text-sm text-gray-800 tracking-[-0.5px] font-medium">
-                  Transações
-                </span>
-                <ChevronDownIcon className="text-gray-900" />
-              </button>
+              <TransactionTypeDropdown />
 
-              <button>
+              <button onClick={handleOpenFiltersModal}>
                 <FilterIcon />
               </button>
             </div>
