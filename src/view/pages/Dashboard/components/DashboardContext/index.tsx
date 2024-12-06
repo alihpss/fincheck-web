b4485b6e159df149ba15d356c0076586efff5dd7
Ali <ali.hps@hotmail.com>
2024-12-06 13:42:05 -0300

    ♻️ Update: Dashboard context
    Add values to the context

--- src/view/pages/Dashboard/components/DashboardContext/index.tsx
+++ src/view/pages/Dashboard/components/DashboardContext/index.tsx
@@ -3,6 +3,13 @@ import React, { createContext, useCallback, useState } from "react";
 interface DashboardContextValue {
   areValuesVisible: boolean;
   toggleValueVisibility: () => void;
+  isNewAccountModalOpen: boolean;
+  openNewAccountModal: () => void;
+  closeNewAccountModal: () => void;
+  isNewTransactionModalOpen: boolean;
+  openNewTransactionModal: (type: "INCOME" | "EXPENSE") => void;
+  closeNewTransactionModal: () => void;
+  newTransactionType: "EXPENSE" | "INCOME" | null;
 }
 
 interface ChildrenProps {
@@ -12,15 +19,50 @@ interface ChildrenProps {
 export const DashboardContext = createContext({} as DashboardContextValue);
 
 export function DashboardProvider({ children }: ChildrenProps) {
-  const [areValuesVisible, setAreValuesVisible] = useState(true);
+  const [areValuesVisible, setAreValuesVisible] = useState(false);
+  const [isNewAccountModalOpen, setIsNewAccountModalOpen] = useState(false);
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  const [newTransactionType, setNewTransactionType] = useState<
+    "EXPENSE" | "INCOME" | null
+  >(null);
 
   const toggleValueVisibility = useCallback(() => {
     setAreValuesVisible((prevState) => !prevState);
   }, []);
 
+  const openNewAccountModal = useCallback(() => {
+    setIsNewAccountModalOpen(true);
+  }, []);
+
+  const closeNewAccountModal = useCallback(() => {
+    setIsNewAccountModalOpen(false);
+  }, []);
+
+  const openNewTransactionModal = useCallback((type: "INCOME" | "EXPENSE") => {
+    setNewTransactionType(type);
+    setIsNewTransactionModalOpen(true);
+  }, []);
+
+  const closeNewTransactionModal = useCallback(() => {
+    setIsNewTransactionModalOpen(false);
+    setNewTransactionType(null);
+  }, []);
+
   return (
     <DashboardContext.Provider
-      value={{ areValuesVisible, toggleValueVisibility }}
+      value={{
+        areValuesVisible,
+        toggleValueVisibility,
+        closeNewAccountModal,
+        isNewAccountModalOpen,
+        openNewAccountModal,
+        isNewTransactionModalOpen,
+        closeNewTransactionModal,
+        openNewTransactionModal,
+        newTransactionType,
+      }}
     >
       {children}
     </DashboardContext.Provider>
