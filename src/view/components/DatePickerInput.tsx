92a4416a23f5d67e72d837bf8bc75816b4b85da9
Ali <ali.hps@hotmail.com>
2024-12-06 13:38:38 -0300

    ✨ Feat: Date picker input

---
+++ src/view/components/DatePickerInput.tsx
@@ -0,0 +1,51 @@
+import { CrossCircledIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
+import { cn } from "../../app/utils/cn";
+import { formatDate } from "../../app/utils/formatDate";
+import { DatePicker } from "./DatePicker";
+import { Popover } from "./Popover";
+
+interface DatePickerInputProps {
+  error?: string;
+  className?: string;
+}
+
+export function DatePickerInput({ className, error }: DatePickerInputProps) {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  return (
+    <div className="">
+      <Popover.Root>
+        <Popover.Trigger>
+          <button
+            type="button"
+            className={cn(
+              "bg-white w-full rounded-lg border border-gray-500 px-3 h-[52px] text-gray-800 placeholder-shown:pt-0 focus:border-gray-700 transition-all outline-none text-left relative pt-4",
+              error && "!border-red-900",
+              className
+            )}
+          >
+            <span className="text-gray-700 text-xs left[-13px] absolute top-2 pointer-events-none">
+              Data
+            </span>
+
+            <span>{formatDate(selectedDate)}</span>
+          </button>
+        </Popover.Trigger>
+
+        <Popover.Content>
+          <DatePicker
+            onChange={(date) => setSelectedDate(date)}
+            value={selectedDate}
+          />
+        </Popover.Content>
+      </Popover.Root>
+
+      {error && (
+        <div className="flex gap-2 items-center mt-2 text-red-900">
+          <CrossCircledIcon />
+          <span className="text-xs">{error}</span>
+        </div>
+      )}
+    </div>
+  );
+}
