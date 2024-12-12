db03684426552b18c98f2d375cfbb4ad54763682
Ali <ali.hps@hotmail.com>
2024-12-12 13:44:18 -0300

    ✨ Feat: Input currency

--- src/view/components/InputCurrency.tsx
+++ src/view/components/InputCurrency.tsx
@@ -1,12 +1,35 @@
+import { CrossCircledIcon } from "@radix-ui/react-icons";
 import { NumericFormat } from "react-number-format";
+import { cn } from "../../app/utils/cn";
 
-export function InputCurrency() {
+interface InputCurrencyProps {
+  error?: string;
+  onChange?: (value: string) => void;
+  value?: string;
+}
+
+export function InputCurrency({ error, onChange, value }: InputCurrencyProps) {
   return (
-    <NumericFormat
-      thousandSeparator="."
-      decimalSeparator=","
-      className="text-gray-800 text-[32px] font-bold tracking-[-1px] w-full outline-none"
-      defaultValue="0"
-    />
+    <div>
+      <NumericFormat
+        decimalScale={2}
+        thousandSeparator="."
+        decimalSeparator=","
+        value={value}
+        className={cn(
+          "text-gray-800 text-[32px] font-bold tracking-[-1px] w-full outline-none",
+          error && "text-red-900"
+        )}
+        onValueChange={({ value }) => {
+          onChange?.(value);
+        }}
+      />
+      {error && (
+        <div className="flex gap-2 items-center mt-2 text-red-900">
+          <CrossCircledIcon />
+          <span className="text-xs">{error}</span>
+        </div>
+      )}
+    </div>
   );
 }
