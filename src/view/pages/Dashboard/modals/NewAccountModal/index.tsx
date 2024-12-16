52e6bdc9d15447959e3d54a7f1b5c5aad1072b8e
Ali <ali.hps@hotmail.com>
2024-12-16 10:39:40 -0300

    ✨ Feat: Account modal controller and modal component

--- src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
+++ src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
@@ -1,3 +1,5 @@
+import { Controller } from "react-hook-form";
+import { Button } from "../../../../components/Button";
 import { ColorsDropdowninput } from "../../../../components/ColorsDropdownInput";
 import { Input } from "../../../../components/Input";
 import { InputCurrency } from "../../../../components/InputCurrency";
@@ -6,37 +8,87 @@ import { Select } from "../../../../components/Select";
 import { useNewAccountModalController } from "./useNewAccountModalController";
 
 export function NewAccountModal() {
-  const { closeNewAccountModal, isNewAccountModalOpen } =
-    useNewAccountModalController();
+  const {
+    closeNewAccountModal,
+    isNewAccountModalOpen,
+    errors,
+    handleSubmit,
+    register,
+    control,
+    isPending,
+  } = useNewAccountModalController();
+
   return (
     <Modal
       title="Nova Conta"
       open={isNewAccountModalOpen}
       onClose={closeNewAccountModal}
     >
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <div>
           <span className="text-gray-600 tracking-[-0.05px] text-xs">
-            Saldo
+            Saldo inicial
           </span>
 
           <div className="flex items-center gap-2">
             <span className="text-gray-600 tracking-[-0.05px] text-lg">R$</span>
-            <InputCurrency />
+
+            <Controller
+              control={control}
+              name="initialBalance"
+              defaultValue="0"
+              render={({ field: { onChange, value } }) => (
+                <InputCurrency
+                  error={errors.initialBalance?.message}
+                  onChange={onChange}
+                  value={value}
+                />
+              )}
+            />
           </div>
         </div>
 
         <div className="mt-10 flex flex-col gap-4">
-          <Input type="text" name="name" placeholder="Nome da Conta" />
-          <Select
-            placeholder="Tipo"
-            options={[
-              { value: "CHECKING", label: "Conta Corrente" },
-              { value: "INVESTMENT", label: "Investimentos" },
-              { value: "CASH", label: "Dinheiro Físico" },
-            ]}
+          <Input
+            type="text"
+            placeholder="Nome da Conta"
+            error={errors.name?.message}
+            {...register("name")}
           />
-          <ColorsDropdowninput />
+
+          <Controller
+            control={control}
+            name="type"
+            defaultValue="CHECKING"
+            render={({ field: { onChange, value } }) => (
+              <Select
+                error={errors.type?.message}
+                placeholder="Tipo"
+                onChange={onChange}
+                value={value}
+                options={[
+                  { value: "CHECKING", label: "Conta Corrente" },
+                  { value: "INVESTMENT", label: "Investimentos" },
+                  { value: "CASH", label: "Dinheiro Físico" },
+                ]}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="color"
+            defaultValue=""
+            render={({ field: { onChange, value } }) => (
+              <ColorsDropdowninput
+                error={errors.color?.message}
+                onChange={onChange}
+                value={value}
+              />
+            )}
+          />
+
+          <Button isLoading={isPending}>Criar</Button>
         </div>
       </form>
     </Modal>
--- src/view/pages/Dashboard/modals/NewAccountModal/useNewAccountModalController.ts
+++ src/view/pages/Dashboard/modals/NewAccountModal/useNewAccountModalController.ts
@@ -1,10 +1,66 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { z } from "zod";
+import { bankAccountService } from "../../../../../app/services/bankAccountsService";
+import { currencyStringToNumber } from "../../../../../app/utils/currencyStringToNumber";
 import { useDashboard } from "../../components/DashboardContext/useDashboard";
+
+const schema = z.object({
+  initialBalance: z.string().min(1, "Saldo inicial é obrigatório"),
+  name: z.string().min(1, "Nome da conta é obrigatório"),
+  type: z.enum(["INVESTMENT", "CASH", "CHECKING"]),
+  color: z.string().min(1, "Cor é obrigatória"),
+});
+
+type FormData = z.infer<typeof schema>;
 
 export function useNewAccountModalController() {
   const { isNewAccountModalOpen, closeNewAccountModal } = useDashboard();
 
+  const {
+    handleSubmit: hookFormHandleSubmit,
+    register,
+    formState: { errors },
+    control,
+    reset,
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const queryClient = useQueryClient();
+  const { isPending, mutateAsync } = useMutation({
+    mutationFn: async (data: FormData) => {
+      const res = await bankAccountService.create({
+        ...data,
+        initialBalance: currencyStringToNumber(data.initialBalance),
+      });
+
+      return res;
+    },
+  });
+
+  const handleSubmit = hookFormHandleSubmit(async (data) => {
+    try {
+      await mutateAsync(data);
+
+      toast.success("Conta cadastrada com sucesso!");
+      closeNewAccountModal();
+      reset();
+      queryClient.invalidateQueries({ queryKey: ["getAllBankAccounts"] });
+    } catch {
+      toast.error("Erro ao criar conta");
+    }
+  });
+
   return {
     isNewAccountModalOpen,
     closeNewAccountModal,
+    register,
+    errors,
+    handleSubmit,
+    control,
+    isPending,
   };
 }
