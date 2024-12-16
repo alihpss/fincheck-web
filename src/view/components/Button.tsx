75786cff7148a786e86647cbb2094ec26d03d728
Ali <ali.hps@hotmail.com>
2024-12-16 09:56:37 -0300

    💄 Button: Variant style

--- src/view/components/Button.tsx
+++ src/view/components/Button.tsx
@@ -4,6 +4,7 @@ import { Spinner } from "./Spinner";
 
 interface ButtonProps extends ComponentProps<"button"> {
   isLoading?: boolean;
+  variant?: "ghost" | "danger";
 }
 
 export function Button({
@@ -11,6 +12,7 @@ export function Button({
   isLoading,
   disabled,
   children,
+  variant,
   ...props
 }: ButtonProps) {
   return (
@@ -21,6 +23,9 @@ export function Button({
         `bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 px-6 h-12 rounded-2xl
           font-medium text-white disabled:text-gray-400 disabled:cursor-not-allowed transition-all
           flex items-center justify-center`,
+        variant === "danger" && "bg-red-900 hover:bg-red-800",
+        variant === "ghost" &&
+          "bg-transparent! border-gray-800 border text-gray-800 hover:bg-gray-800/5",
         className
       )}
     >
