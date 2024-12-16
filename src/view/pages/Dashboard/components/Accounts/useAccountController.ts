98f7e4b7a4946b035aaa2d9c121e1422fa4b8ae8
Ali <ali.hps@hotmail.com>
2024-12-16 10:57:45 -0300

    ✨ Feat: Account controller

--- src/view/pages/Dashboard/components/Accounts/useAccountController.ts
+++ src/view/pages/Dashboard/components/Accounts/useAccountController.ts
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useMemo, useState } from "react";
 import { useWindowWidth } from "../../../../../app/hooks/useWindowWidth";
 import { useDashboard } from "../DashboardContext/useDashboard";
+import { bankAccountService } from "./../../../../../app/services/bankAccountsService/index";
 
 export function useAccountsController() {
   const windowWidth = useWindowWidth();
@@ -12,14 +14,28 @@ export function useAccountsController() {
     isEnd: false,
   });
 
+  const { data = [], isLoading } = useQuery({
+    queryKey: ["getAllBankAccounts"],
+    queryFn: bankAccountService.getAll,
+  });
+
+  const currentBalance = useMemo(() => {
+    if (!data) {
+      return 0;
+    }
+
+    return data.reduce((total, account) => total + account.currentBalance, 0);
+  }, [data]);
+
   return {
     sliderState,
     setSliderState,
     windowWidth,
     toggleValueVisibility,
     areValuesVisible,
-    isLoading: false,
-    accounts: [],
     openNewAccountModal,
+    isLoading,
+    accounts: data,
+    currentBalance,
   };
 }
